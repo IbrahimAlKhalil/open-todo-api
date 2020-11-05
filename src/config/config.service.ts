813a052b505d26584cffc2aa145c068d5e031bbf
ibrahim <hm.ibrahimalkhalil@gmail.com>
2020-11-06 00:11:00 +0600

    refactor: remove env property and isDev method from Config Service

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -3,12 +3,6 @@ import db from './config/db';
 
 @Injectable()
 export class Config {
-  env = process.env.NODE_ENV;
-
   // Configuration data goes here
   db = db();
-
-  isDev(): boolean {
-    return process.env.NODE_ENV === 'development';
-  }
 }
