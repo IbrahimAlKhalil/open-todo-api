ce492e0a58eda70a6efd6f25160a9669248a9e26
ibrahim <hm.ibrahimalkhalil@gmail.com>
2020-11-05 23:05:55 +0600

    chore: enable CORS and install/use helmet express middleware

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV === 'production') {
+    const options: CorsOptions = {
+      methods: ['GET', 'POST', 'OPTIONS', 'HEAD'],
+    };
+
+    if (process.env.CORS_ORIGIN) {
+      options.origin = process.env.CORS_ORIGIN
+        .split(',')
+        .map(origin => origin.trim());
+    }
+
+    app.enableCors(options);
+    app.use(helmet());
+  }
+
   await app.listen(3000);
 }
 
