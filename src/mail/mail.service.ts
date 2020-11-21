9f4d02d9e113ecfc6a3667d687781e92e890d7d2
ibrahim <hm.ibrahimalkhalil@gmail.com>
2020-11-22 01:05:42 +0600

    chore(mail): make transport property of MailService private and add send method

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,9 +1,21 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { MAIL_TRANSPORT } from '../constants';
+import { SentMessageInfo } from 'nodemailer';
 import Mail from 'nodemailer/lib/mailer';
 
 @Injectable()
 export class MailService {
-  constructor(@Inject(MAIL_TRANSPORT) public transport: Mail) {
+  constructor(@Inject(MAIL_TRANSPORT) private transport: Mail) {
   }
+
+  send(options: MailOptions): Promise<SentMessageInfo> {
+    return this.transport.sendMail(options);
+  }
+}
+
+export interface MailOptions {
+  subject: string;
+  html: string;
+  from: string;
+  to: string;
 }
