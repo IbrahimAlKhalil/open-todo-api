043076cfeaae349b11f2c80d50edb0ee495bec56
ibrahim <hm.ibrahimalkhalil@gmail.com>
2020-11-21 22:47:11 +0600

    chore(user): add createUser method to UserService with unit test

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,18 +1,63 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { EntityManager, Repository } from 'typeorm';
 import { UserService } from './user.service';
+import { Gender, User } from './user.entity';
 
 describe('UserService', () => {
+  let userRepo: Repository<User>;
   let service: UserService;
+  const data = {
+    firstName: 'Ibrahim',
+    lastName: 'Al Khalil',
+    username: 'IbrahimAlKhalil',
+    email: 'dummyemail@dummy.com',
+    password: '!234567$',
+    gender: Gender.male,
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: {
+            create: jest.fn(() => data),
+            save: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<UserService>(UserService);
+    service = module.get(UserService);
+    userRepo = module.get(
+      getRepositoryToken(User),
+    );
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('.createUser()', () => {
+    it('should use Transactional Entity Manager if given', async () => {
+      const trxRepo = {
+        save: jest.fn(),
+        create: jest.fn(() => data),
+      };
+
+      const entityManager = {
+        getRepository() {
+          return trxRepo;
+        },
+      };
+
+      await service.createUser(data, entityManager as unknown as EntityManager);
+
+      expect(trxRepo.save).toBeCalledWith(data);
+    });
+
+    it('should create user', async () => {
+      await service.createUser(data);
+
+      expect(userRepo.save).toBeCalledWith(data);
+    });
   });
 });
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,18 @@
+import { EntityManager, Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Injectable } from '@nestjs/common';
+import { User } from './user.entity';
 
 @Injectable()
 export class UserService {
+  constructor(@InjectRepository(User) private userRepo: Repository<User>) {
+  }
+
+  createUser(data: Pick<User, 'firstName' | 'lastName' | 'gender' | 'username' | 'email' | 'password'>, entityManager?: EntityManager): Promise<User> {
+    const repository = entityManager?.getRepository(User) || this.userRepo;
+
+    return repository.save(
+      repository.create(data),
+    );
+  }
 }
