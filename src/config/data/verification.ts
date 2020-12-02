f7e11558d27902aa353ea225f1ed1bb39efab93b
ibrahim <hm.ibrahimalkhalil@gmail.com>
2020-12-02 23:46:50 +0600

    chore(config): add tokenAudience and tokenSubject members in verification configuration

--- src/config/data/verification.ts
+++ src/config/data/verification.ts
@@ -5,5 +5,7 @@ export default function() {
     subject: env.VERIFICATION_SUBJECT,
     from: env.VERIFICATION_FROM,
     tokenLifetime: env.VERIFICATION_TOKEN_LIFETIME,
+    tokenAudience: env.VERIFICATION_TOKEN_AUDIENCE,
+    tokenSubject: env.VERIFICATION_TOKEN_SUBJECT,
   };
 };
